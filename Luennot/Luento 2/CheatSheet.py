09cbd9c5f21c99ef7e2611c2a993966fbe1d190c
Leevi Tyykä <leevi.tyyska@gmail.com>
2025-09-16 20:28:55 +0300

    resolve merge conflicts

---
+++ Luennot/Luento 2/CheatSheet.py
@@ -0,0 +1,37 @@
+print("Hello world!")
+print("Here we are")
+
+print("He said: \"Hello\", and kept walking")
+print("He said Hello \n \r \t \b and \\'kept walking")
+
+examleString = "Text and numb3rs"
+exampleInt = 12
+exampleFloat = 12.12
+exampleBoolean = True
+
+#Firstname = input("What is your first name: ")
+#Lastname = input("What is your last name: ")
+#print("Hello ", Firstname, Lastname)
+
+Sentence = "Finding substring"
+print(Sentence)
+print(Sentence[0]) #antaa lauseen ensimmäisen merkkin
+print(Sentence[-2]) #antaa lauseen toisiksi viimeisen merkin
+print(Sentence[2:9]) #antaa lauseen kolmannesta merkistä 8. merkkiin asti
+print(Sentence[:9]) #antaa lauseen 8. merkkiin asti
+print(Sentence[-9:-2]) #antaa lauseen 9. viimeisestä merkistä 2. viimeiseen merkkiin asti
+print(Sentence[2:9:3]) #antaa lauseen 3. 9. ja 4. merkit
+print(Sentence[::3]) #antaa lauseen joka 3. merkin 
+print(Sentence[::-1]) #antaa lauseen takaperin
+
+#name = "Leevi"
+#age = 26
+
+#print(name + " " + str(age))
+
+num1 = input("Anna luku 1 ")
+num2 = input("Anna luku 2 ")
+
+num3 = int(num1) + int(num2)
+print(num3)
+
